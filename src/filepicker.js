74ab09e7f8e1a6b182e5845c921c82f9b3c06a8d
Erik Schlegel <erik.schlegel@gmail.com>
2015-05-13 14:49:09 -0700

    initial commit

--- src/filepicker.js
+++ src/filepicker.js
@@ -9,10 +9,6 @@ class ReactPicker extends React.Component {
   componentDidMount: function() {
   	
   	this.dropzone = new Dropzone(this.getDOMNode(), options);
-
-    return {
-      isDragActive: false
-    }
   },
 
   render: function () {
