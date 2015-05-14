ea06478609f8ea3f9c09424b97af420e2756d262
Felix Rieseberg <felix@felixrieseberg.com>
2015-05-14 11:39:04 -0700

    Cleanup

--- src/filepicker.js
+++ src/filepicker.js
@@ -7,38 +7,41 @@ var IconComponent = require('./icon');
 
 class ReactFilePicker extends React.Component {
 
-  componentDidMount() {
-  	var self = this;
+    componentDidMount() {
+        var self = this;
 
-  	if(!this.props.config.postUrl)
-  		throw new Error("postUrl is a required react property for this component");
+        if (!this.props.config.postUrl) {
+            throw new Error('postUrl is a required react property for this component');
+        }
 
-  	let options =  {
-  		            url: this.props.config.postUrl,
-  		            headers: {"Access-Control-Allow-Credentials" : true,
-  		            		  "Access-Control-Allow-Headers" : "Content-Type, X-Requested-With, X-PINGOTHER, X-File-Name, Cache-Control",
-  		            		  "Access-Control-Allow-Methods" : "PUT, POST, GET, OPTIONS",
-  		            		  "Access-Control-Allow-Origin" : "*"
-  		        			 },
-  		        	withCredentials: true
-  	               };
-  	this.dropzone = new Dropzone(React.findDOMNode(self), options);
-  	Dropzone.autoDiscover= false;
-  }
+        let options = {
+            url: this.props.config.postUrl,
+            headers: {
+                'Access-Control-Allow-Credentials': true,
+                'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With, X-PINGOTHER, X-File-Name, Cache-Control',
+                'Access-Control-Allow-Methods': 'PUT, POST, GET, OPTIONS',
+                'Access-Control-Allow-Origin': '*'
+            },
+            withCredentials: true
+        };
 
-  componentWillUnmount() {
-     this.dropzone.destroy();
-  }
+        this.dropzone = new Dropzone(React.findDOMNode(self), options);
+        Dropzone.autoDiscover = false;
+    }
 
-  render() {
-        var icon = (this.props.fileicon) ? <IconComponent filetype="txt" /> : null;
+    componentWillUnmount() {
+        this.dropzone.destroy();
+    }
+
+    render() {
+        var icon = (this.props.fileicon) ? < IconComponent filetype = 'txt' / > : null;
 
         return (
-            <div className="filepicker dropzone">
+            <div className='filepicker dropzone'> 
                 {icon}
             </div>
         );
-  }
+    }
 };
 
 module.exports = ReactFilePicker;
