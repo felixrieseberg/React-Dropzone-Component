66c2fc6d9e7de6f43d53df7826c09667e998d77a
Erik Schlegel <erik.schlegel@gmail.com>
2015-05-14 16:18:55 -0700

    Added Multer Impl with Grunt Express Server for processing uploaded files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path'),
     multer = require('multer'),
     bodyParser = require('body-parser'),
+    MulterImpl = require('./src/multerImpl'),
     app = module.exports = express();
     app.set('port', process.env.PORT || 3000);
 
@@ -12,11 +13,8 @@ var path = require('path'),
     }, 50);
 
     app.use(express.static('./'));
-    app.use(multer({dest: 'uploads'})); // dest is not necessary if you are happy with the default: /tmp
-
-    app.get('/hello', function(req, res) {
-      res.send('Hello');
-    });
+    //app.use(multer({dest: 'uploads'})); // dest is not necessary if you are happy with the default: /tmp
+    app.use(new MulterImpl({}).init());
 
     app.post('/uploadHandler', function (req, res) {
         var files = req.files.file;
