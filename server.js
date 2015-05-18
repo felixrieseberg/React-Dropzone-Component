d8f99464182eaddf69b077a2a86a2fa0dde6ecdd
Felix Rieseberg <felix@felixrieseberg.com>
2015-05-18 12:39:38 -0700

    Prep for Azure

--- server.js
+++ server.js
@@ -2,15 +2,11 @@ var express = require('express'),
     path = require('path'),
     multer = require('multer'),
     bodyParser = require('body-parser'),
-    MulterImpl = require('./src-server/multerImpl'),
     app = module.exports = express();
     app.set('port', process.env.PORT || 3000);
 
 
 app.use(express.static('./'));
-// Dest is not necessary if you are happy with the default: /tmp
-// app.use(multer({ dest: 'uploads' }));
-app.use(new MulterImpl({}).init());
 
 app.post('/uploadHandler', function (req, res) {
     res.sendStatus(200);
