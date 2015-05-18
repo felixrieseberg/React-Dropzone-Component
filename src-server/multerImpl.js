9bf6b6958b091bbd3d34349f928492f442c39d12
Felix Rieseberg <felix@felixrieseberg.com>
2015-05-18 12:25:09 -0700

    Code Style for Multer

--- src-server/multerImpl.js
+++ src-server/multerImpl.js
@@ -1,25 +1,25 @@
-function MulterImpl(config){
-   var defaultDest = './uploads/';
+function MulterImpl(config) {
+    var defaultDest = './uploads/';
 
-   this.init = function(){
-      var multer  = require('multer');
-      var uploadDir = !config.uplodaDir ? defaultDest : config.uplodaDir;
+    this.init = function () {
+        var multer = require('multer');
+        var uploadDir = !config.uplodaDir ? defaultDest : config.uplodaDir;
 
-      var options = {
-          dest: uploadDir,
-          rename: function (fieldname, filename) {
-                              return filename+Date.now();
-          },
-          onFileUploadStart: function (file) {
-                              console.log(file.originalname + ' is starting ...');
-          },
-          onFileUploadComplete: function (file) {
-                               console.log(file.fieldname + ' uploaded to  ' + file.path);
-          }
-      };
+        var options = {
+            dest: uploadDir,
+            rename: function (fieldname, filename) {
+                return filename + Date.now();
+            },
+            onFileUploadStart: function (file) {
+                console.log(file.originalname + ' is starting ...');
+            },
+            onFileUploadComplete: function (file) {
+                console.log(file.fieldname + ' uploaded to  ' + file.path);
+            }
+        };
 
-      return multer(options);
-   }
+        return multer(options);
+    }
 }
 
 module.exports = MulterImpl;
