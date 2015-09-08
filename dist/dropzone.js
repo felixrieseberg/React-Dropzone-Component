086553509ddc70a76fe00126121552dfb5c06226
Felix Rieseberg <felix@felixrieseberg.com>
2015-09-08 09:57:26 -0700

    Require Dropzone in "ComponentdidMount"
    This should enable server-side rendering. Thanks to @tomchentw, who
    pointed me to @cainvommars’s solution.

--- dist/dropzone.js
+++ dist/dropzone.js
@@ -1,15 +1,14 @@
 /*!
- * react-dropzone-component 0.4.4 - 
+ * react-dropzone-component 0.5.2 (dev build at Tue, 08 Sep 2015 16:56:07 GMT) - 
  * MIT Licensed
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ReactDropzone = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
 var React = (typeof window !== "undefined" ? window['React'] : typeof global !== "undefined" ? global['React'] : null),
-    Dropzone = require('dropzone'),
     Helpers = require('./helpers'),
     IconComponent = require('./icon'),
-    DropzoneComponent;
+    Dropzone, DropzoneComponent;
 
 DropzoneComponent = React.createClass({displayName: "DropzoneComponent",
     /**
@@ -53,11 +52,13 @@ DropzoneComponent = React.createClass({displayName: "DropzoneComponent",
         var self = this,
             options = this.getDjsConfig();
 
+        Dropzone = Dropzone || require('dropzone');
+        Dropzone.autoDiscover = false;
+
         if (!this.props.config.postUrl && !this.props.eventHandlers.drop) {
             console.info('Neither postUrl nor a "drop" eventHandler specified, the React-Dropzone component might misbehave.');
         }
 
-        Dropzone.autoDiscover = false;
         this.dropzone = new Dropzone(React.findDOMNode(self), options);
         this.setupEvents();
     },
