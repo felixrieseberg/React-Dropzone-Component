1185c0d5a147edd15e6bd2a26058249783e45483
Felix Rieseberg <felix@felixrieseberg.com>
2016-03-13 09:58:41 +0700

    :wrench: Change Entry Point

--- index.js
+++
@@ -1,9 +0,0 @@
-var exported;
-
-if (require('react') && require('react-dom')) {
-    exported = require('./lib/react-dropzone.js');
-} else {
-    exported = require('./dist/dropzone.js');
-}
-
-module.exports = exported;
